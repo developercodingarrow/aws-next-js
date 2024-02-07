92f3b10c92d1e2e12eb0f6c982c0bd6d8c7ece3b
developercodingarrow <digitalcloudinfo@gmail.com>
2024-02-08 00:14:07 +0530

    saransh first commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
-// next.config.js
+const path = require("path");
 
 module.exports = {
-  // Other configurations...
   server: {
     port: 3000,
+  },
+  webpack: (config, { isServer }) => {
+    // Add your custom webpack configuration here
+    // Example: Add a custom alias
+    config.resolve.alias["@components"] = path.join(__dirname, "components");
+
+    // Example: Modify babel loader configuration
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
   },
 };
