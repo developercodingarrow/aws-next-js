b320857e65d7bfa32e806025849f2800638cb02e
developercodingarrow <digitalcloudinfo@gmail.com>
2024-02-07 15:11:00 +0530

    next.config added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "39f7cd2bdccc618b"; }
+/******/ 		__webpack_require__.h = function() { return "ac8dbedd5bf3b898"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+
+module.exports = {
+  // Other configurations...
+  server: {
+    port: 3000,
+  },
+};
