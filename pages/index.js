f3237be007cdde7ebc263f684740d291fd26d88c
developercodingarrow <digitalcloudinfo@gmail.com>
2024-02-09 16:24:29 +0530

    test backend api

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Head from "next/head";
 import Image from "next/image";
@@ -8,17 +8,19 @@ import styles from "../styles/home.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     try {
-  //       const response = await axios.get("http://13.60.28.193/api/aws");
-  //       console.log(response.data); // Log the response data to the console
-  //     } catch (error) {
-  //       console.error("Error fetching data:", error);
-  //     }
-  //   }
-  //   fetchData();
-  // }, []);
+  const [message, setmessage] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get("http://13.201.99.72:8000/api/aws");
+        console.log(response.data); // Log the response data to the console
+        setmessage(response.data.status);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchData();
+  }, []);
   return (
     <>
       <Head>
@@ -28,7 +30,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <h1>hello Next js</h1>
+        <h1>hello Next js change</h1>
+        <h1>api message {message} </h1>
       </main>
     </>
   );
